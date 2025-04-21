d7bc211bbe5ebb4bc730cd304ff2cee62bef49db
SeneSatka <senesatka@gmail.com>
2025-04-21 22:44:44 +0300

    enhance store with task management functions

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,11 +1,14 @@
+import { add } from 'date-fns'
+import { t } from 'i18next'
 import { defineStore } from 'pinia'
 type Modals="edit"|"create"|"delete"|"settings"|"none"
 type Theme = "light" | "dark"
+export type Task={ date: number, title: string, completed: boolean }
 export interface State {
   openedModal: Modals,
   theme: Theme,
   lng: string,
-  todos: Array<{ date: number, title: string, completed: boolean }>
+  todos: Array<Task>
 }
 export const useStore= defineStore('main', {
   state: ():State => {
@@ -37,7 +40,21 @@ export const useStore= defineStore('main', {
     setLng(lng: string) {
       this.lng = lng;
     },
-},
+    addTask(title: string) {
+      const date = Date.now();
+      this.todos.push({
+        date: date,
+        title: title,
+        completed: false,
+      });
+    },
+    deleteTask(id:number) {
+      this.todos = this.todos.filter((task) => task.date !== id);
+    },
+    editTask(id:number,data:Task){
+this.deleteTask(id)
+this.todos.push(data)
+  }},
 persist:{
   omit:['openedModal'],
   storage: localStorage,
